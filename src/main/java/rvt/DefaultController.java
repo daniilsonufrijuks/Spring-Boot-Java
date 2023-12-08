d159a66ede4ae8f121c7a112949d762d833e7813
daniilsonufrijuks <do00492@rvt.lv>
2023-12-08 11:48:13 +0000

    about

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -8,8 +8,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class DefaultController {
     
-   @GetMapping(value = "/")
-    String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
-        return "index";
+//    @GetMapping(value = "/")
+//     // String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
+//     //     return "index";
+//     // }
+
+//     String index(Model model) {
+//         int age = 99;
+//         String school = "RVT";
+//         model.addAttribute("age", age);
+//         model.addAttribute("school", school);
+//         return "index";
+//     }
+
+
+    @GetMapping(value = "/about")
+    String hello(Model model2) {
+        String name = "Dannils";
+        String surename = "Onufrijuks";
+        int age = 17;
+        String group = "DP 2-1";
+        model2.addAttribute("name", name);
+        model2.addAttribute("surename", surename);
+        model2.addAttribute("age", age);
+        model2.addAttribute("group", group);
+
+        return "about";
     }
 }
