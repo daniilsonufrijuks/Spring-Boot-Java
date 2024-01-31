5120e80765029de74fcb29f603e4748e4c710279
daniilsonufrijuks <do00492@rvt.lv>
2024-01-31 12:19:15 +0000

    bin search

--- src/main/java/rvt/exercises/BinarySearch.java
+++ src/main/java/rvt/exercises/BinarySearch.java
@@ -3,29 +3,41 @@ package rvt.exercises;
 import java.util.ArrayList;
 
 public class BinarySearch {
+    public static void sort(int[] array) {
+        for (int i = array.length-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j  + 1] = temp;
+                } 
+            }
+        }
+    }
     public static int BinSearch(int[] array, int searched) {
         int i = 0;
         int end = array.length - 1;
-        int middle = (end + i) / 2;
-        for (;i > end;) {
+        int middle = 0;
+        for (;i < end;) {
             middle = (end + i) / 2;
-            if (array[middle] == array[searched]) {
+            if (array[middle] == searched) {
                 return middle+1;
             }
 
-            if (array[middle] < array[searched]) {
-                i = middle - 1;
+            if (array[middle] < searched) {
+                i = middle + 1;
             }
 
-            if (array[middle] > array[searched]) {
-                i = middle - 1;
+            if (array[middle] > searched) {
+                end = middle - 1;
             }
         }
-        return middle+1;
+        return middle;
     }
 
     public static void main(String[] args) {
-        int[] numbers = {6, 5, 8, 7, 11};
-        System.out.println(BinSearch(numbers, 11));
+        int[] numbers = {6, 5, 8, 7};
+        sort(numbers);
+        System.out.println(BinSearch(numbers, 5));
     }
 }
