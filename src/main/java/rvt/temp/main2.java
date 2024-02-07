6b8a0b1f2ee333051087ce4eaecf573f88a91347
daniilsonufrijuks <do00492@rvt.lv>
2024-02-07 13:01:14 +0000

    person and counter

--- src/main/java/rvt/temp/main2.java
+++ src/main/java/rvt/temp/main2.java
@@ -2,14 +2,14 @@ package rvt.temp;
 
 public class main2 {
     
-public static void main(String[] args) {
-    Student student = new Student("Kenny", 23, 140);
+    public static void main(String[] args) {
+        Student student = new Student("Kenny", 23, 140);
 
-    Person person = student;
-    Object object = student;
+        Person person = student;
+        Object object = student;
 
-    System.out.print(student + ", ");
-    System.out.print(person + ", ");
-    System.out.println(object);
-}
+        System.out.print(student + ", ");
+        System.out.print(person + ", ");
+        System.out.println(object);
+    }
 }
